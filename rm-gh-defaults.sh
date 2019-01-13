18b9ee7d4792f1544eff5d915128d44ad2c15f8d
Lars Kruse <lakruzz@Larss-Mac-mini.local>
2019-01-13 13:17:47 +0100

    close #80 remove the functions from rm gh defaults

--- rm-gh-defaults.sh
+++ rm-gh-defaults.sh
@@ -22,34 +22,17 @@ then
   exit $?
 fi
 
-
-rm-label()
-{
-  label=$1
+for label in "bug" "duplicate" "enhancement" "good first issue" "help wanted" "invalid" "question" "wontfix"
+do
   echo Label: \""$label"\"
-  if in-use "$label"
+  if ghi list -L "$label" | grep "^None.$" > /dev/null 2>&1
   then
+    ghi label -D "$label"
+    echo
+  else
     echo WARNING: $label is in use - can not be deleted!
     echo Delete it manually with \'ghi label -D \"$label\"\'
     echo
-  else
-    ghi label -D "$label"
-    echo
   fi
-}
 
-in-use()
-{
-  label=$1
-  if ghi list -L "$label" | grep "^None.$" > /dev/null 2>&1
-  then
-    return 1
-  else
-    return 0
-  fi
-}
-
-for label in "bug" "duplicate" "enhancement" "good first issue" "help wanted" "invalid" "question" "wontfix"
-do
-  rm-label "$label"
 done
